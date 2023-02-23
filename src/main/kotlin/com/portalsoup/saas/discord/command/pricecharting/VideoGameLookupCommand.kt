11a93ed82ce361947e600219881ac8893cf3e390
portalsoup <portalsoup@gmail.com>
2023-02-22 21:50:22 -0800

    Remove unused code

--- src/main/kotlin/com/portalsoup/saas/discord/command/pricecharting/VideoGameLookupCommand.kt
+++ src/main/kotlin/com/portalsoup/saas/discord/command/pricecharting/VideoGameLookupCommand.kt
@@ -2,14 +2,8 @@ package com.portalsoup.saas.discord.command.pricecharting
 
 import com.portalsoup.saas.core.Logging
 import com.portalsoup.saas.core.db.execAndMap
-import com.portalsoup.saas.data.tables.pricecharting.VideoGame
-import com.portalsoup.saas.data.tables.pricecharting.VideoGamePrice
-import com.portalsoup.saas.data.tables.pricecharting.VideoGamePriceTable
 import com.portalsoup.saas.discord.command.Command
 import discord4j.core.event.domain.message.MessageCreateEvent
-import discord4j.core.`object`.entity.channel.MessageChannel
-import discord4j.core.spec.EmbedCreateSpec
-import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -71,44 +65,8 @@ object VideoGameLookupCommand: Command, Logging {
     private fun formatGameString(game: FoundVideoGame): String = "" +
             "${game.name} - ${takeIf { game.consoles.size > 1 }?.let { "\n\t" } ?: "" }" +
             game.consoles.map { "${it.key}: ${it.value ?: ""}" }.joinToString("\n\t")
-
-    private fun getPriceOfGame(game: VideoGame): VideoGamePrice? = transaction {
-        game.id
-            .let { VideoGamePriceTable.select { VideoGamePriceTable.videoGameId eq it } }
-            .sortedBy { VideoGamePriceTable.createdOn }
-            .map(VideoGamePrice::fromRow)
-            .also { println("Got the prices of the following game:\n ${it.joinToString("\n")}") }
-            .firstOrNull()
-    }
-
-    fun embed(game: VideoGame): EmbedCreateSpec {
-        return EmbedCreateSpec.builder()
-            .title(game.productName)
-            .addField("Console", game.consoleName, false)
-//            .addField("Loose price", game.)
-            .build()
-    }
-
-    fun gameNotFound(it: MessageChannel): Mono<Void> {
-        return it.createMessage("").then()
-    }
 }
 
 data class Result(val pricechartingId: String, val name: String, val console: String, val price: String?, val priceDate: LocalDate)
 
 data class FoundVideoGame(val name: String, val consoles: Map<String, String?>)
-
-/*
-
-SELECT
-  DISTINCT vg.pricecharting_id,
-  vg.product_name <-> 'chrono trigger' as dist,
-  vg.product_name,
-  vg.console_name,
-  loose_price
-FROM
-  video_game as vg
-  LEFT OUTER JOIN video_game_price ON video_game_price.video_game_id = vg.pricecharting_id
-ORDER by dist LIMIT 25;
-
-*/
