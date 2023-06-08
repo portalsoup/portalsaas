43f673696a1fe6a5cc1a84ca26a27ff9b54ac959
portalsoup <portalsoup@gmail.com>
2023-06-07 19:29:44 -0700

    Removed old versions of code

--- src/main/kotlin/com/portalsoup/saas/discord/command/application/global/ApplicationCommand.kt
+++
@@ -1,9 +0,0 @@
-package com.portalsoup.saas.discord.command.application.global
-
-import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
-import org.reactivestreams.Publisher
-
-interface ApplicationCommand {
-
-    fun handleEvent(event: ChatInputInteractionEvent): Publisher<Void>?
-}
